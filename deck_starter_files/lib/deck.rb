0c534b75f6cc5c40a08d2d947f19c122eac82615
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-15 19:10:27 -0400

    Completed tests 1 & 2.

--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,6 +1,50 @@
 require_relative "card"
+require "pry-byebug"
+
 
 class Deck
+  def initialize
+    @deck = []
+    #need to loop ranks and suits
+    @ranks = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+    @suits = [:clubs, :diamonds, :hearts, :spades]
+    deck_builder
+  end
+
+  def deck_builder
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        card = Card.new(rank, suit)
+        @deck << card
+      end
+    end
+  end
+
+  def cards_left
+    52
+  end
+
+
+
+  def draw
+    Card.new(:A, :clubs) 
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
 
