3e6372a6768716f4a6573c7263e31a2a31b41d6f
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-15 15:47:18 -0400

    Added sudo code to deck and defined rank hash for cards

--- deck_starter_files/lib/card.rb
+++ deck_starter_files/lib/card.rb
@@ -1,2 +1,28 @@
+=begin
+11 numbers (Ace = 1)
+A Face values
+A Suit
+=end
+
 class Card
+  def that_card_has_a_suit(card.suit)
+    card_hash = {
+      diamonds: [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
+      clubs: [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
+      hearts: [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
+      spades: [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
+    }
+    puts card.suit
+  end
+
+  def that_card_has_a_rank(card.rank)
+    rank_hash = {
+      [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king]
+    }
+    puts card.rank
+  end
+
+
+
 end
+
--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,12 @@
 require_relative "card"
 
 class Deck
+
 end
+
+=begin
+A Deck has:
+52 Cards
+4 Suits - Hearts, Spades, Diamonds, Clubs
+13 Ranks (lowest to highest) - Ace-King
+=end
